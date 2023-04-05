b16226b9747480e48449879cb92fd3c303ef67d1
Giuliano Kranevitter <info@giulianok.com>
2023-04-05 07:29:06 -0400

    chore: typescript-library-startup

---
+++ src/tryCatch.ts
@@ -0,0 +1,22 @@
+export function tryCatch<Params = unknown, Result = unknown>({
+  tryer,
+  catcher
+}: {
+  tryer: (params: Params) => Result;
+  catcher: (params: Params, message: string) => Result;
+}) {
+  return (params: Params) => {
+    try {
+      return tryer(params);
+    } catch (error) {
+
+      let message = 'Something went wrong'
+
+      if (error instanceof Error) {
+        message = error.message
+      }
+
+      return catcher(params, message);
+    }
+  };
+}
