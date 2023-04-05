57778e05e7a0653c3d8fd125c98791cb20df8e31
Giuliano Kranevitter <info@giulianok.com>
2023-04-05 08:11:21 -0400

    chore: added husky

--- src/tryCatch.ts
+++ src/tryCatch.ts
@@ -1,6 +1,6 @@
 export function tryCatch<Params = unknown, Result = unknown>({
   tryer,
-  catcher
+  catcher,
 }: {
   tryer: (params: Params) => Result;
   catcher: (params: Params, message: string) => Result;
@@ -9,11 +9,10 @@ export function tryCatch<Params = unknown, Result = unknown>({
     try {
       return tryer(params);
     } catch (error) {
-
-      let message = 'Something went wrong'
+      let message = "Something went wrong";
 
       if (error instanceof Error) {
-        message = error.message
+        message = error.message;
       }
 
       return catcher(params, message);
